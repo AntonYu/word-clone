9473e998a13144ef09e80d9420f23811fb967786
Anton Yurchenko <webbymasta@gmail.com>
2023-04-13 22:44:29 +0400

    ex1 fixes

--- src/components/Game/Form.js
+++ src/components/Game/Form.js
@@ -1,38 +1,44 @@
 import React from "react";
 
-const MAX_SYMBOLS = 5;
+const WORD_SYMBOLS_COUNT = 5;
 
-export default function Form({ addGuess, isInputDisabled }) {
-  const [input, setInput] = React.useState("");
+export default function Form({addGuess, isInputDisabled}) {
+    const [input, setInput] = React.useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    if (input.length !== MAX_SYMBOLS) return;
+        if (input.length !== WORD_SYMBOLS_COUNT) {
+            window.alert(`Please input exactly ${WORD_SYMBOLS_COUNT} symbols 🐝`);
+            return;
+        }
 
-    addGuess(input);
-    setInput("");
-  };
+        addGuess(input);
+        setInput("");
+    };
 
-  const handleChange = (e) => {
-    if (e.target.value.length > MAX_SYMBOLS) return;
+    const handleChange = (e) => {
+        if (e.target.value.length > WORD_SYMBOLS_COUNT) return;
 
-    setInput(e.target.value.toUpperCase());
-  };
+        setInput(e.target.value.toUpperCase());
+    };
 
-  return (
-    <div>
-      <form className="guess-input-wrapper" onSubmit={handleSubmit}>
-        <label htmlFor="guess-input">Enter guess:</label>
-        <input
-          id="guess-input"
-          type="text"
-          value={input}
-          onChange={handleChange}
-          pattern={`[a-zA-Z]{${MAX_SYMBOLS}}`}
-          disabled={isInputDisabled}
-        />
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+                <label htmlFor="guess-input">Enter guess:</label>
+                <input
+                    required
+                    id="guess-input"
+                    type="text"
+                    value={input}
+                    onChange={handleChange}
+                    minLength={WORD_SYMBOLS_COUNT}
+                    maxLength={WORD_SYMBOLS_COUNT}
+                    pattern={`[a-zA-Z]{${WORD_SYMBOLS_COUNT}}`}
+                    disabled={isInputDisabled}
+                />
+            </form>
+        </div>
+    );
 }
